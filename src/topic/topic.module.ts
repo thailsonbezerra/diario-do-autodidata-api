1732dffdf4fa3f32820e211d7eab0cc101097aec
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-05-26 21:20:02 -0300

    feat: adicionado cache na busca por topicBySubjecId

--- src/topic/topic.module.ts
+++ src/topic/topic.module.ts
@@ -3,9 +3,10 @@ import { TopicController } from './topic.controller';
 import { TopicService } from './topic.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TopicEntity } from './entity/topic.entity';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TopicEntity])],
+  imports: [CacheModule.register(), TypeOrmModule.forFeature([TopicEntity])],
   controllers: [TopicController],
   providers: [TopicService],
 })
--- src/topic/topic.service.ts
+++ src/topic/topic.service.ts
@@ -1,20 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TopicEntity } from './entity/topic.entity';
 import { Repository } from 'typeorm';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class TopicService {
   constructor(
     @InjectRepository(TopicEntity)
     private readonly topicRepository: Repository<TopicEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async getAllTopicsBySubjectId(subjectId: number): Promise<TopicEntity[]> {
-    return await this.topicRepository.find({
+    const topicsCache: TopicEntity[] = await this.cacheManager.get(
+      `topic_${subjectId}`,
+    );
+
+    if (topicsCache) {
+      return topicsCache;
+    }
+
+    const topics = await this.topicRepository.find({
       where: {
         subjectId,
       },
     });
+
+    await this.cacheManager.set(`topic_${subjectId}`, topics);
+
+    return topics;
   }
 }

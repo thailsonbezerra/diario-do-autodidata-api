9670cbec3288abea0a3145aae08326a808e103cd
Thailson Bezerra <thailsonbezerra@outlook.com>
2023-06-08 23:12:42 -0300

    feat: utilizando jwt para login do user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,19 @@ import { UserEntity } from 'src/user/entity/user.entity';
 import { UserService } from 'src/user/user.service';
 import { loginDto } from './dtos/login.dto';
 import { compare } from 'bcrypt';
+import { ReturnLogin } from './dtos/returnLogin.dto';
+import { ReturnUserDto } from 'src/user/dtos/returnUser.dto';
+import { JwtService } from '@nestjs/jwt';
+import { LoginPayload } from './dtos/loginPayload.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async login(loginDto: loginDto): Promise<UserEntity> {
+  async login(loginDto: loginDto): Promise<ReturnLogin> {
     const user: UserEntity | undefined = await this.userService
       .findUserByEmail(loginDto.email)
       .catch(() => undefined);
@@ -19,6 +26,11 @@ export class AuthService {
       throw new NotFoundException('Email or password invalid');
     }
 
-    return user;
+    return {
+      accessToken: await this.jwtService.signAsync({
+        ...new LoginPayload(user),
+      }),
+      user: new ReturnUserDto(user),
+    };
   }
 }
---
+++ src/auth/dtos/loginPayload.dto.ts
@@ -0,0 +1,11 @@
+import { UserEntity } from 'src/user/entity/user.entity';
+
+export class LoginPayload {
+  id: number;
+  name: string;
+
+  constructor(user: UserEntity) {
+    this.id = user.id;
+    this.name = user.name;
+  }
+}
